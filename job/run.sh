f4b209e9ef460b105c829f832d71dc7ed47b18b7
definitelyuncertain <saikirann94@gmail.com>
2017-10-21 13:32:50 +0000

    Create run.sh

---
+++ job/run.sh
@@ -0,0 +1,6 @@
+export PATH="/usr/local/nvidia/bin:$PATH"
+export LD_LIBRARY_PATH="/usr/local/nvidia/lib:$LD_LIBRARY_PATH"
+source /tools/config.sh
+source activate py35
+cd /home/test/job
+python -u convolutional.py &> out
