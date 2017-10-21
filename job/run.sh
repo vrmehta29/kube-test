59d9f3c65d50136299e0eb1a25ece636e7148354
definitelyuncertain <saikirann94@gmail.com>
2017-10-21 14:40:52 +0000

    Update run.sh

--- job/run.sh
+++ job/run.sh
@@ -2,5 +2,5 @@ export PATH="/usr/local/nvidia/bin:$PATH"
 export LD_LIBRARY_PATH="/usr/local/nvidia/lib:$LD_LIBRARY_PATH"
 source /tools/config.sh
 source activate py35
-cd /home/test/job
+cd /home/test/kube-test/job
 python -u convolutional.py &> out
